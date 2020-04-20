d45daafe3ef93190c7e2099990de265d5b48b834
konkeong <konkeong@gmail.com>
2020-04-20 18:25:00 +0800

    After route.

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,19 +3,31 @@ export const products = [
     id: 101,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens.'
   },
   {
     id: 201,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras.'
   },
   {
     id: 301,
     name: 'Phone Standard',
     price: 299,
     description: ''
+  },
+  {
+    id: 401,
+    name: 'Newow Phone',
+    price: 599,
+    description: 'A new and wow phone.'
+  },
+  {
+    id: 501,
+    name: 'Paper Phone',
+    price: 0.99,
+    description: 'A mock phone made of pape.'
   }
 ];
 
